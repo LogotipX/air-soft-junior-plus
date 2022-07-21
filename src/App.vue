d0959041f730d738d945f1a12fd5b38a97a7fad2
LogotipX <Starikov1000@gmail.com>
2022-07-21 22:24:43 +0500

    refactor: rewrite all structure to recursion

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,227 @@
 <template>
-  Array nesting: {{ nodesArrNesting }}
   <div class="nodes__container">
-    <ul
-      v-for="(nodeFromLevel, nodeLevel) in structuredNodes"
-      :key="nodeFromLevel"
-      class="nodes__leveling"
-    >
-      <li v-for="node in nodeFromLevel" :key="node" class="node__blocks">
-        <node-block
-          :title="node.title"
-          :isFirstNode="nodeLevel === 0"
-          :nodes="node.nodes"
-        />
-        {{ nodeLevel }}
-      </li>
-    </ul>
+    <div v-for="node in nodesArr" :key="node" class="branch">
+      <branches-container :node="node" :isFirstNode="true" />
+    </div>
   </div>
 </template>
 
 <script>
-import NodeBlock from "./components/NodeBlock.vue";
 import { getNodes } from "./api.js";
+import BranchesContainer from "./components/BranchesContainer.vue";
 
 export default {
   name: "App",
   components: {
-    NodeBlock,
+    BranchesContainer,
   },
   data() {
     return {
-      nodesArrNesting: 0,
-      structuredNodes: [],
       nodesArr: [
-        // {
-        //   nodes: [
-        //     {
-        //       nodes: [
-        //         {
-        //           nodes: [
-        //             {
-        //               nodes: [
-        //                 {
-        //                   nodes: [{ nodes: null, title: "5 Node 1.1.1.1.1.1" }],
-        //                   title: "4 Node 1.1.1.1.1",
-        //                 },
-        //                 {
-        //                   nodes: [{ nodes: null, title: "5 Node 1.1.1.1.2.1" }],
-        //                   title: "4 Node 1.1.1.1.2",
-        //                 },
-        //               ],
-        //               title: "3 Node 1.1.1.1",
-        //             },
-        //           ],
-        //           title: "2 Node 1.1.1",
-        //         },
-        //       ],
-        //       title: "1 Node 1.1",
-        //     },
-        //     {
-        //       nodes: [
-        //         {
-        //           nodes: [
-        //             {
-        //               nodes: [
-        //                 {
-        //                   nodes: [{ nodes: null, title: "5 Node 1.2.1.1.2.1" }],
-        //                   title: "4 Node 1.2.1.1.2",
-        //                 },
-        //                 {
-        //                   nodes: [{ nodes: null, title: "5 Node 1.2.1.1.1.2" }],
-        //                   title: "4 Node 1.2.1.1.1",
-        //                 },
-        //               ],
-        //               title: "3 Node 1.2.1.1",
-        //             },
-        //           ],
-        //           title: "2 Node 1.2.1",
-        //         },
-        //       ],
-        //       title: "1 Node 1.2",
-        //     },
-        //   ],
-        //   title: "0 Node 1",
-        // },
+        {
+          nodes: [
+            {
+              nodes: [
+                {
+                  nodes: [
+                    {
+                      nodes: [
+                        {
+                          nodes: [
+                            {
+                              nodes: [
+                                {
+                                  nodes: [
+                                    {
+                                      nodes: null,
+                                      title: "5 Node 1.1.1.1.1.1",
+                                    },
+                                  ],
+                                  title: "4 Node 1.1.1.1.1",
+                                },
+                                {
+                                  nodes: [
+                                    {
+                                      nodes: null,
+                                      title: "5 Node 1.1.1.1.2.1",
+                                    },
+                                  ],
+                                  title: "4 Node 1.1.1.1.2",
+                                },
+                              ],
+                              title: "5 Node 1.1.1.1.1.1",
+                            },
+                          ],
+                          title: "4 Node 1.1.1.1.1",
+                        },
+                        {
+                          nodes: [
+                            {
+                              nodes: [
+                                {
+                                  nodes: [
+                                    {
+                                      nodes: [
+                                        {
+                                          nodes: null,
+                                          title: "5 Node 1.1.1.1.2.1",
+                                        },
+                                      ],
+                                      title: "4 Node 1.1.1.1.2",
+                                    },
+                                  ],
+                                  title: "5 Node 1.1.1.1.1.1",
+                                },
+                              ],
+                              title: "5 Node 1.1.1.1.2.1",
+                            },
+                          ],
+                          title: "4 Node 1.1.1.1.2",
+                        },
+                      ],
+                      title: "3 Node 1.1.1.1",
+                    },
+                  ],
+                  title: "2 Node 1.1.1",
+                },
+              ],
+              title: "1 Node 1.1",
+            },
+            {
+              nodes: [
+                {
+                  nodes: [
+                    {
+                      nodes: [
+                        {
+                          nodes: [{ nodes: null, title: "5 Node 1.2.1.1.1.2" }],
+                          title: "4 Node 1.2.1.1.1",
+                        },
+                      ],
+                      title: "3 Node 1.2.1.1",
+                    },
+                  ],
+                  title: "2 Node 1.2.1",
+                },
+              ],
+              title: "1 Node 1.2",
+            },
+          ],
+          title: "0 Node 1",
+        },
+        {
+          nodes: [
+            {
+              nodes: [
+                {
+                  nodes: [
+                    {
+                      nodes: [
+                        {
+                          nodes: [{ nodes: null, title: "5 Node 1.1.1.1.1.1" }],
+                          title: "4 Node 1.1.1.1.1",
+                        },
+                        {
+                          nodes: [
+                            {
+                              nodes: [
+                                {
+                                  nodes: [
+                                    {
+                                      nodes: [
+                                        {
+                                          nodes: [
+                                            {
+                                              nodes: null,
+                                              title: "5 Node 1.1.1.1.1.1",
+                                            },
+                                          ],
+                                          title: "4 Node 1.1.1.1.1",
+                                        },
+                                        {
+                                          nodes: [
+                                            {
+                                              nodes: null,
+                                              title: "5 Node 1.1.1.1.2.1",
+                                            },
+                                          ],
+                                          title: "4 Node 1.1.1.1.2",
+                                        },
+                                      ],
+                                      title: "5 Node 1.1.1.1.1.1",
+                                    },
+                                  ],
+                                  title: "4 Node 1.1.1.1.1",
+                                },
+                                {
+                                  nodes: [
+                                    {
+                                      nodes: [
+                                        {
+                                          nodes: [
+                                            {
+                                              nodes: null,
+                                              title: "5 Node 1.1.1.1.1.1",
+                                            },
+                                          ],
+                                          title: "4 Node 1.1.1.1.1",
+                                        },
+                                        {
+                                          nodes: [
+                                            {
+                                              nodes: null,
+                                              title: "5 Node 1.1.1.1.2.1",
+                                            },
+                                          ],
+                                          title: "4 Node 1.1.1.1.2",
+                                        },
+                                      ],
+                                      title: "5 Node 1.1.1.1.2.1",
+                                    },
+                                  ],
+                                  title: "4 Node 1.1.1.1.2",
+                                },
+                              ],
+                              title: "5 Node 1.1.1.1.2.1",
+                            },
+                          ],
+                          title: "4 Node 1.1.1.1.2",
+                        },
+                      ],
+                      title: "3 Node 1.1.1.1",
+                    },
+                  ],
+                  title: "2 Node 1.1.1",
+                },
+              ],
+              title: "1 Node 1.1",
+            },
+            {
+              nodes: [
+                {
+                  nodes: [
+                    {
+                      nodes: [
+                        {
+                          nodes: [{ nodes: null, title: "5 Node 1.2.1.1.2.1" }],
+                          title: "4 Node 1.2.1.1.2",
+                        },
+                        {
+                          nodes: [{ nodes: null, title: "5 Node 1.2.1.1.1.2" }],
+                          title: "4 Node 1.2.1.1.1",
+                        },
+                      ],
+                      title: "3 Node 1.2.1.1",
+                    },
+                  ],
+                  title: "2 Node 1.2.1",
+                },
+              ],
+              title: "1 Node 1.2",
+            },
+          ],
+          title: "0 Node 1",
+        },
       ],
     };
   },
@@ -93,42 +232,7 @@ export default {
 
   methods: {
     async getNodesArr() {
-      const nodesFromApi = await getNodes();
-      // console.log(nodesFromApi);
-      this.nodesArr = nodesFromApi;
-      this.getArrayNestingLevel(this.nodesArr);
-      this.prepareStructuredNodes();
-      this.createStructuredNodesArray();
-      // console.log(this.structuredNodes);
-    },
-
-    getArrayNestingLevel(arr, level = 0) {
-      if (Array.isArray(arr)) {
-        level += 1;
-        arr.forEach((node) => {
-          this.getArrayNestingLevel(node.nodes, level);
-        });
-      } else {
-        if (level >= this.nodesArrNesting) {
-          this.nodesArrNesting = level;
-        }
-      }
-    },
-
-    prepareStructuredNodes() {
-      for (let i = 0; i < this.nodesArrNesting; i++) {
-        this.structuredNodes.push([]);
-      }
-    },
-
-    createStructuredNodesArray(arr = this.nodesArr, curLvl = 0) {
-      arr.forEach((arrNode) => {
-        this.structuredNodes[curLvl].push(arrNode);
-
-        if (arrNode.nodes !== null) {
-          this.createStructuredNodesArray(arrNode.nodes, curLvl + 1);
-        }
-      });
+      this.nodesArr = await getNodes();
     },
   },
 };
@@ -152,20 +256,6 @@ export default {
 }
 
 .nodes__container {
-  display: flex;
-  height: 720px;
   align-items: center;
-
-  .nodes__leveling {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    height: 100%;
-    margin: 0 2.5%;
-
-    .nodes__blocks {
-      margin: 5% 0;
-    }
-  }
 }
 </style>
---
+++ src/components/BranchesContainer.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="branch">
+    <div class="input-node-block">
+      <node-block
+        :title="node.title"
+        :isFirstNode="isFirstNode"
+        :nodes="node.nodes"
+      />
+    </div>
+    <ul class="output-node-blocks">
+      <template v-if="node.nodes">
+        <li
+          v-for="(nodeItem, idx) in node.nodes"
+          :key="nodeItem + idx"
+          class="node-branch"
+        >
+          <branches-container :node="nodeItem" />
+        </li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<script>
+import NodeBlock from "./NodeBlock.vue";
+
+export default {
+  name: "BranchesContainer",
+  components: { NodeBlock },
+  props: {
+    node: {
+      type: Object,
+      require: false,
+    },
+    isFirstNode: {
+      type: Boolean,
+      require: false,
+      default: false,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.branch {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: fit-content;
+
+  & > * {
+    margin: 20px;
+  }
+}
+</style>
